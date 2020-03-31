6f1ff284eecd367790fd01454e8718b14a0bc5c7
njdevengine <24538036+njdevengine@users.noreply.github.com>
2020-03-31 03:57:43 +0000

    Update get_all_csvs_in_subdirectories.py

--- get_all_csvs_in_subdirectories.py
+++ get_all_csvs_in_subdirectories.py
@@ -1,6 +1,13 @@
 import os
 import os.path
 
+files = []
 for dirpath, dirnames, filenames in os.walk("."):
     for filename in [f for f in filenames if f.endswith(".csv")]:
-        print(os.path.join(dirpath, filename))
+        files.append(os.path.join(dirpath, filename))
+        
+frames = []
+
+for i in files:
+    df = pd.read_csv(i)
+    frames.append(df)
